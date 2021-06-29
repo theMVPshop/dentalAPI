f8f1d5ea5fd421d3a79930ca519545792c88eab9
Matt Carter <carter.matthew@icloud.com>
2021-06-28 21:14:25 -0400

    update sql statements

--- controllers/offices.js
+++ controllers/offices.js
@@ -5,7 +5,7 @@ const { handleSQLError } = require('../SQL/error');
 //@TYPE GET
 //@DESC gets all records in db
 const getAllRecords = (req, res) => {
-  pool.query(`SELECT * FROM heroku_dfd78b6b7e89374.offices`, (err, rows) => {
+  pool.query(`SELECT * FROM offices`, (err, rows) => {
     if (err) return handleSQLError(res, err);
     return res.json(rows);
   });
@@ -14,7 +14,7 @@ const getAllRecords = (req, res) => {
 //@TYPE GET
 //@DESC gets records by city
 const allByCity = (req, res) => {
-  let sql = `SELECT * FROM heroku_dfd78b6b7e89374.offices WHERE city LIKE ?`;
+  let sql = `SELECT * FROM offices WHERE city LIKE ?`;
   sql = mysql.format(sql, [`${req.params.city}%`]);
 
   pool.query(sql, (err, rows) => {
@@ -26,7 +26,7 @@ const allByCity = (req, res) => {
 //@TYPE GET
 //@DESC gets records by city AND state
 const allByCityState = (req, res) => {
-  let sql = `SELECT * FROM heroku_dfd78b6b7e89374.offices WHERE city = ? AND state = ?`;
+  let sql = `SELECT * FROM offices WHERE city = ? AND state = ?`;
   sql = mysql.format(sql, [req.params.city, req.params.state]);
 
   pool.query(sql, (err, rows) => {
@@ -41,7 +41,7 @@ const allByCityState = (req, res) => {
 //@TYPE GET route
 //@DES gets all records by state
 const allByState = (req, res) => {
-  let sql = `SELECT * FROM heroku_dfd78b6b7e89374.offices WHERE state = ?`;
+  let sql = `SELECT * FROM offices WHERE state = ?`;
   sql = mysql.format(sql, [req.params.state]);
 
   pool.query(sql, (err, rows) => {
@@ -53,7 +53,7 @@ const allByState = (req, res) => {
 //@TYPE GET route
 //@DESC gets all records by zip
 const allByZip = (req, res) => {
-  let sql = `SELECT * FROM heroku_dfd78b6b7e89374.offices WHERE zip = ? `;
+  let sql = `SELECT * FROM offices WHERE zip = ? `;
   sql = mysql.format(sql, [req.params.zip]);
 
   pool.query(sql, (err, rows) => {
